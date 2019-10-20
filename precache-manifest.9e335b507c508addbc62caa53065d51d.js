255fa04db7852fa2ff5b2fdcdf1aab86cb8f38ed
andrey_mardash <andrey.mardash@gmail.com>
2019-10-20 19:58:48 +0300

    Updates

---
+++ precache-manifest.9e335b507c508addbc62caa53065d51d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cea9865387c67ee619de66f7731963",
+    "url": "/Labyrinth/index.html"
+  },
+  {
+    "revision": "95ff1864a03842732855",
+    "url": "/Labyrinth/static/css/main.e679848a.chunk.css"
+  },
+  {
+    "revision": "c156172a76fe5ed10b5d",
+    "url": "/Labyrinth/static/js/2.3769205e.chunk.js"
+  },
+  {
+    "revision": "95ff1864a03842732855",
+    "url": "/Labyrinth/static/js/main.6e190ea2.chunk.js"
+  },
+  {
+    "revision": "d338051b4b62e8bf596d",
+    "url": "/Labyrinth/static/js/runtime-main.945d90fb.js"
+  }
+]);
