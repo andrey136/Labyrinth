cf19ea111c575956cd71df0a8f4eaa80a151cbbf
andrey_mardash <andrey.mardash@gmail.com>
2019-10-20 11:40:18 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,33 @@ import './App.css';
 
 class App extends Component {
   state = {
+    location: [0, 0],
     dir: 0,
+    walls: [{height: 400, left: 242.5, top: 0}],
+    width: 50,
+    field: 500
   };
 
-  animation(){
-    console.log('animation');
+  animation(dir) {
+    let left = this.state.location[0];
+    let top = this.state.location[1];
+    switch(dir){
+      case 0:
+        left = 0;
+        break;
+      case 1:
+        left = this.state.walls[0].left - this.state.width;
+        break;
+      case 2:
+        top = 0;
+        break;
+      case 3:
+        top = this.state.field - this.state.width;
+    }
     this.setState({
-      dir: !this.state.dir,
+      location: [left, top],
     });
+    console.log(this.state.location);
   }
 
   render() {
@@ -30,10 +49,18 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className="game">
-            <img src={ball} className={this.state.dir ? 'ball-right' : 'ball-left'} alt="" width="50" height="50"/>
+            <img src={ball} className="ball"
+                 style={{
+                   transition: `margin 0.7s`,
+                   margin: `${this.state.location[1]}px 0 0 ${this.state.location[0]}px`,
+                 }}
+                 alt="" width="50" height="50"/>
+            <div className="wall-1"></div>
           </section>
-          <button onClick={() => this.animation()}>left</button>
-          <button onClick={() => this.animation()}>Right</button>
+          <button onClick={() => this.animation(0)}>Left</button>
+          <button onClick={() => this.animation(1)}>Right</button>
+          <button onClick={() => this.animation(2)}>Top</button>
+          <button onClick={() => this.animation(3)}>Bottom</button>
         </div>
       </div>
     );
