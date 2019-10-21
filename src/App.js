032502235bba175cdce7a82d9ffd122ec12a58a7
andrey_mardash <andrey.mardash@gmail.com>
2019-10-21 19:35:04 +0300

    Forth Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import ball from './photos/soccerBall.png';
 import './App.css';
 import uniqid from 'uniqid';
+import coin from './photos/coin.png';
+import gold from './photos/gold.png';
+
 
 
 class App extends Component {
@@ -10,6 +13,8 @@ class App extends Component {
     dir: 0,
     vertical_walls: [{height: 420, left: 0, top: 0},{height: 205, left: 65, top: 80},{height: 70, left: 115, top: 350}, {height: 50, left: 250, top: 15}, {height: 70, left: 250, top: 215}, {height: 50, left: 335, top: 300}, {height: 120, left: 400, top: 80}, {height: 70, left: 420, top: 350}, {height: 435, left: 485, top: 65}],//, {height: 400, left: 200, top: 0}, {height: 100, left: 300, top: 50}, {height: 340, left: 350, top: 50}],
     horizontal_walls: [{size: 485, left: 15, top: 0}, {size: 135, left: 65, top: 65}, {size: 165, left: 250, top: 65},  {size: 165, left: 250, top: 200},  {size: 200, left: 65, top: 285},  {size: 150, left: 335, top: 285},  {size: 150, left: 200, top: 350},  {size: 435, left: 0, top: 420},  {size: 485, left: 0, top: 485}],
+    coins: [{left: 435, top: 435},{left: 15, top: 80},{left: 350, top: 150}, {left: 265, top: 15}],
+    gold:[{left: 200, top: 235}],
     width: 50,
     widthOfWalls: 15,
   };
@@ -55,6 +60,13 @@ class App extends Component {
     return theNearestWall;
   }
 
+  money(left, top, dir){
+    if(dir < 2){
+
+    } else {
+
+    }
+  }
 
 animation(dir)
 {
@@ -75,6 +87,7 @@ animation(dir)
     case 3:
       wall === null ? top = 450 : top = wall.top - this.state.width;
   }
+  this.money(left, top, dir);
   this.setState({
     location: [left, top],
   });
@@ -106,11 +119,15 @@ render()
                                                     width: this.state.widthOfWalls,height: el.height}}></div> )}
           {this.state.horizontal_walls.map(el => <div key={uniqid()} className="wall" style={{margin: `${el.top}px 0 0 ${el.left}px`,
             width: el.size,height: this.state.widthOfWalls}}></div> )}
+          {this.state.coins.map(el => <img src={coin} key={uniqid()} className="coin" style={{margin: `${el.top}px 0 0 ${el.left}px`, width: 50, height: 50}}></img>)}
+          <img src={gold} className="gold" style={{margin: `${this.state.gold[0].top}px 0 0 ${this.state.gold[0].left}px`}} alt=""/>
         </section>
-        <button onClick={() => this.animation(0)}>Left</button>
-        <button onClick={() => this.animation(1)}>Right</button>
-        <button onClick={() => this.animation(2)}>Top</button>
-        <button onClick={() => this.animation(3)}>Bottom</button>
+        <div className="buttons">
+          <button onClick={() => this.animation(0)}>Left</button>
+          <button onClick={() => this.animation(1)}>Right</button>
+          <button onClick={() => this.animation(2)}>Top</button>
+          <button onClick={() => this.animation(3)}>Bottom</button>
+        </div>
       </div>
     </div>
   );
